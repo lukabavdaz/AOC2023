f5b628b6a7a8eb8e8378746192478e780407482d
lukabavdaz <lukabavdaz@gmail.com>
2023-12-01 23:42:58 +0100

    Single solution day01
    Using generics for code reuse.

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -6,11 +6,12 @@ fn get_input() -> Vec<String> {
         .collect()
 }
 
-fn part1(input: &[String]) -> u32 {
+fn part1(input: impl IntoIterator<Item = impl AsRef<str>>) -> u32 {
     input
-        .iter()
+        .into_iter()
         .map(|l| {
-            l.chars()
+            l.as_ref()
+                .chars()
                 .filter_map(|c| c.to_digit(10))
                 .collect::<Vec<u32>>()
         })
@@ -18,56 +19,21 @@ fn part1(input: &[String]) -> u32 {
         .sum()
 }
 
-//Should be faster for longer strings
-//Could be more efficient by extracting the digit check out of find_number
-fn part2_v1(input: &[String]) -> u32 {
-    input
+fn part2(input: &[String]) -> u32 {
+    part1(input.iter().map(|l| {
+        [
+            "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+        ]
         .iter()
-        .map(|l| {
-            let first = (1..=l.len()).find_map(|x| find_number(&l[x.saturating_sub(5)..x]));
-            let last = (1..=l.len())
-                .find_map(|x| find_number(&l[l.len() - x..l.len().min(l.len() - x + 5)]));
-            10 * first.unwrap() + last.unwrap()
+        .enumerate()
+        .fold(l.clone(), |acc, (i, s)| {
+            acc.replace(s, &format!("{s}{}{s}", i + 1))
         })
-        .sum()
-}
-
-fn find_number(ss: &str) -> Option<u32> {
-    [
-        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-    ]
-    .iter()
-    .enumerate()
-    .position(|(i, n)| ss.contains(n) || ss.contains(char::from_digit(i as u32 + 1, 10).unwrap()))
-    .map(|i| (i + 1) as u32)
-}
-
-//Simple solution, faster for my input
-fn part2_v2(input: &[String]) -> u32 {
-    input
-        .iter()
-        .map(|l| {
-            [
-                "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-            ]
-            .iter()
-            .enumerate()
-            .fold(l.clone(), |acc, (i, s)| {
-                acc.replace(s, &format!("{s}{}{s}", i + 1))
-            })
-        })
-        .map(|l| {
-            l.chars()
-                .filter_map(|c| c.to_digit(10))
-                .collect::<Vec<u32>>()
-        })
-        .map(|l| 10 * l[0] + l[l.len() - 1])
-        .sum()
+    }))
 }
 
 fn main() {
     let input = get_input();
     println!("part1: {}", part1(&input));
-    println!("part2_v1: {}", part2_v1(&input));
-    println!("part2_v2: {}", part2_v2(&input));
+    println!("part2_v2: {}", part2(&input));
 }
