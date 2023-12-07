75c4cc85db228ec7a02f05deb9fb1d6eca273486
lukabavdaz <lukabavdaz@gmail.com>
2023-12-07 15:32:17 +0100

    Cleanup day07
    Using the squares of occurrences rather than a map to determine the ordering.

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -14,18 +14,7 @@ fn part1(input: &[(String, u64)]) -> u64 {
         for c in s.chars() {
             *map.entry(c).or_insert(0) += 1;
         }
-        let mut occurrences = map.into_values().collect::<Vec<_>>();
-        occurrences.sort();
-        let set = 1000000
-            * match &occurrences[..] {
-            &[.., 5] => 6,
-            &[.., 4] => 5,
-            &[.., 2, 3] => 4,
-            &[.., 3] => 3,
-            &[.., 2, 2] => 2,
-            &[.., 2] => 1,
-            _ => 0,
-        };
+        let set = map.values().map(|o| o * o * 1000000).sum::<u64>();
 
         let values = [
             '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A',
@@ -37,7 +26,7 @@ fn part1(input: &[(String, u64)]) -> u64 {
     cards
         .iter()
         .enumerate()
-        .map(|(i, &(_, v))| (i + 1) as u64 * v)
+        .map(|(i, &(_, b))| (i + 1) as u64 * b)
         .sum()
 }
 
@@ -49,23 +38,14 @@ fn part2(input: &[(String, u64)]) -> u64 {
             *map.entry(c).or_insert(0) += 1;
         }
         let mut occurrences = map.into_values().collect::<Vec<_>>();
-        occurrences.sort();
 
-        if occurrences.len() == 0 {
-            occurrences.push(5);
+        occurrences.sort();
+        if let Some(o) = occurrences.last_mut() {
+            *o += s.chars().filter(|&x| x == 'J').count();
         } else {
-            *occurrences.last_mut().unwrap() += s.chars().filter(|&x| x == 'J').count();
+            occurrences.push(5);
         }
-        let set = 1000000
-            * match &occurrences[..] {
-                &[.., 5] => 6,
-                &[.., 4] => 5,
-                &[.., 2, 3] => 4,
-                &[.., 3] => 3,
-                &[.., 2, 2] => 2,
-                &[.., 2] => 1,
-                _ => 0,
-            };
+        let set = occurrences.iter().map(|o| (o * o) as u64 * 1000000).sum::<u64>();
 
         let values = [
             'J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A',
@@ -77,7 +57,7 @@ fn part2(input: &[(String, u64)]) -> u64 {
     cards
         .iter()
         .enumerate()
-        .map(|(i, &(_, v))| (i + 1) as u64 * v)
+        .map(|(i, &(_, b))| (i + 1) as u64 * b)
         .sum()
 }
 
